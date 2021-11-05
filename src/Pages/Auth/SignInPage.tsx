beb66c4f2f548633e1adb39b0e65b7e5d6bfeaaa
OganesK <53556592+OganesK@users.noreply.github.com>
2021-11-05 22:25:16 +0300

    signup preset added

--- src/Pages/Auth/SignInPage.tsx
+++ src/Pages/Auth/SignInPage.tsx
@@ -1,11 +1,145 @@
 import React from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Copyright from "./Copyright";
+import useStyles from "./Style";
+import { withStyles } from '@material-ui/core/styles';
 
 class SignInPage extends React.Component {
     render(){
+        const { classes } = this.props;
         return(
-            <h1>Signin page</h1>
+            <Grid container component="main" className={classes.root}>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          style={{
+            backgroundImage: `url(${HeaderImage})`,
+          }}
+          className={classes.image}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={(e: any): void => {
+                e.preventDefault();
+                const userInput: LogInInput = {
+                  login: emailFormValue,
+                  password: passwordFormValue,
+                };
+                logInHandler({
+                  variables: {
+                    input: userInput,
+                  },
+                })
+                  .then()
+                  .catch(err => console.error(err));
+              }}
+              noValidate
+            >
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                value={emailFormValue}
+                onChange={(e: any): void => setEmailFormValue(e.target.value)}
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                className={classes.textField}
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                  },
+                }}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e: any): void => setPaswordFormValue(e.target.value)}
+                value={passwordFormValue}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" />}
+                label="Remember me"
+                className={classes.controlLabel}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                className={classes.submitButton}
+                onClick={(e: any): void => {
+                  e.preventDefault();
+                  const userInput: LogInInput = {
+                    login: emailFormValue,
+                    password: passwordFormValue,
+                  };
+                  logInHandler({
+                    variables: {
+                      input: userInput,
+                    },
+                  })
+                    .then()
+                    .catch(err => console.error(err));
+                }}
+              >
+                Log In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link className={classes.link} href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link className={classes.link} href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    </Grid>
         )
     }
 }
 
-export default SignInPage;
+export default withStyles(useStyles)(SignInPage);
